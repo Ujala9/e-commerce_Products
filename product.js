0d7fb43c4b31bdca24c8ee2703e85e1a47b54649
techei <ujalabharti7816@gmail.com>
2025-07-31 14:54:17 +0530

    fix: removing product.json agian

--- product.js
+++ product.js
@@ -12,32 +12,33 @@ app.use(cors());
 app.use(express.json());
 
 //Load JSON file
-const products = JSON.parse(fs.readFileSync("./product.json", "utf-8"));
+// const products = JSON.parse(fs.readFileSync("./product.json", "utf-8"));
 
-const seedProductsData = async () => {
-  try {
-      await Product.deleteMany({});
+// const seedProductsData = async () => {
+//   try {
+//       await Product.deleteMany({});
 
-      for (const item of products) {
-        const newEvent = new Product({
-            name: item.name,
-            brand: item.brand,
-            category: item.category,
-            price: item.price,
-            rating: item.rating,
-            image: item.image,
-            description: item.description
-        });
-        await newEvent.save();
-        //console.log(item.name)
-      }
+//       for (const item of products) {
+//         const newEvent = new Product({
+//             name: item.name,
+//             brand: item.brand,
+//             category: item.category,
+//             price: item.price,
+//             rating: item.rating,
+//             image: item.image,
+//             description: item.description
+//         });
+//         await newEvent.save();
+//         //console.log(item.name)
+//       }
     
-    } catch (error) {
-      console.error("Error seeding events:", error);
-    //   process.exit(1);
-    }
-  };
+//     } catch (error) {
+//       console.error("Error seeding events:", error);
+//     //   process.exit(1);
+//     }
+//   };
   
+// this function is to seed data
   //seedProductsData();
   
 app.get("/", async (req, res) => {
